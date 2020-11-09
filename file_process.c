5649cd1631d5831bd666c9c86eaf8aaad8a3f419
AISSAOUI SIHAM <saissaoui@etu.unistra.fr>
2020-11-10 00:41:41 +0100

    process

--- file_process.c
+++ file_process.c
@@ -23,49 +23,55 @@ typedef struct name_server
 int main(int argc, char *argv[])
 {
 	int len, fd, i = 0;
+	ssize_t n;
 	char *buff;
 	char *occur;
 
-	struct root_server * rs_tab;
-	if((rs_tab = malloc(10000*sizeof(root_server)))==NULL){
-		perror("malloc");
-	}
+	struct root_server *rs_tab;
+	rs_tab = malloc(1000 * sizeof(root_server));
 
 	if (argc != 2)
 	{
-		printf("Usage : ./file_process <file_name>");
+		perror("Usage : ./file_process <file_name>");
 	}
 
-	if ((fd = open(argv[1], O_RDONLY)) == -1)
-	{
-		perror("open file");
-	}
+	fd = open(argv[1], O_RDONLY);
 
 	len = lseek(fd, 0, SEEK_END);
-	lseek(fd,SEEK_SET,0);
+	lseek(fd, SEEK_SET, 0);
 	printf("file size : %d", len);
 
-	if(read(fd, &buff, len)==-1){
-		perror("read");
-	}
-
-	printf("read ok");
-
-	char temp[6];
-	while(buff[0] != '\0')
+	while ((n = read(fd, &buff, 1)) > 0)
 	{
-		struct root_server rs;
-		occur = strchr(buff, '|');
-		occur[0] = '\0';
-		memcpy(rs_tab[i].addr_ip, buff, occur - buff + 1);
-		buff = occur + 1;
-		occur = strchr(buff, '\n');
-		occur[0] = '\0';
-		memcpy(temp, buff, occur - buff + 1);
-		rs_tab[i].port = atoi(temp);
-		buff = occur + 1;
-		i++;
+		if (buff[n] == '|' )
+		{
+			struct root_server rs;
+			memcpy(rs_tab[i].addr_ip, buff, n);
+			lseek(fd, SEEK_CUR, 1);
+		}
+		if (buff[n] == '\n'){
+			char temp[6];
+			memcpy(temp, buff, n);
+			rs_tab[i].port = atoi(temp);
+			lseek(fd, SEEK_CUR, 1);
+			i++;
+		}
 	}
 
+	// while (buff[0] != '\0')
+	// {
+	// 	struct root_server rs;
+	// 	occur = strchr(buff, '|');
+	// 	occur[0] = '\0';
+	// 	memcpy(rs_tab[i].addr_ip, buff, occur - buff + 1);
+	// 	buff = occur + 1;
+	// 	occur = strchr(buff, '\n');
+	// 	occur[0] = '\0';
+	// 	memcpy(temp, buff, occur - buff + 1);
+	// 	rs_tab[i].port = atoi(temp);
+	// 	buff = occur + 1;
+	// 	i++;
+	// }
 	close(fd);
+	return 0;
 }
